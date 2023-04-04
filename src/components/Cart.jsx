aaadf9a1858931a72a5b830649a0eb596ed63165
Hunter305 <66633649+Hunter305@users.noreply.github.com>
2023-04-04 15:33:15 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,7 +12,7 @@ const Cart = (props) => {
             return (
               <div className="col mb-5" key={`${index}`}>
                 <div className="card h-100">
-                  <img className="card-img-top" src={value.imgSrc} alt="..." />
+                  <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
 
                   <div className="card-body p-4">
                     <div className="text-center">
